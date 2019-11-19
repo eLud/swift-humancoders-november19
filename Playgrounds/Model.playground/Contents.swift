2f8da902df28a93ba9c27990bd3fb174a358291c
Ludovic Ollagnier <ludovic@ollagnier.me>
2019-11-19 11:39:16 +0100

    Demo enums + Optional

--- Playgrounds/Model.playground/Contents.swift
+++ Playgrounds/Model.playground/Contents.swift
@@ -1,23 +1,102 @@
-import UIKit
+struct Training: Equatable {
 
-var str = "Hello, playground"
+    // Enums can have rawValue : String, Characters, Int / Double / Float
+    enum Style: Int {
+        // We can refine the rawValue by making it explicit
+        case onSite = 0
+        case remote = 1
 
-struct Truc {
+        //Enums can have functions, even inits
+        func test() -> String {
+            switch self {
+            case .onSite:
+                return "sur site"
+            case .remote:
+                return "a distance"
+            }
+        }
+    }
 
+    let theme: String
+    let duration: Int
+    var isFull: Bool
+    var trainer: Trainer? // Optional<Trainer>
+    let style: Style
 }
 
-class TrucManager {
+struct Trainer: Equatable {
+    fileprivate var firstName: String
 
-    var tab = ["str", "ter"]
+    // Func with no parameter nor return
+    func train() {
+        print("I train")
+    }
 
-    func add(_ elemnt: String) {
-        tab.append(elemnt)
+    /// Func with parameters no return
+    func train(in training: Training, during duration: Int) {
+        print("I train in \(training) during \(duration) days.")
+    }
+
+    func train(in training: Training, during duration: Double) {
+        print("I train in \(training) during \(duration) days.")
+    }
+
+    func program(for training: Training) -> String? {
+
+        guard training.trainer == self else { return nil }
+        return "a program"
     }
 }
 
-enum CompassPoint {
-    case north
-    case south
-    case east
-    case west
+/// This manages trainings
+class TrainingManager {
+
+    var trainings: [Training] = [] // Array<Training>
+
+    func add(_ training: Training) {
+        trainings.append(training)
+    }
+
+    /// Removes a training
+    /// - Parameter training: the Training to remove
+    func remove(_ training: Training) {
+        guard let index = trainings.firstIndex(of: training) else { return }
+        trainings.remove(at: index)
+    }
 }
+
+var training1 = Training(theme: "iOS", duration: 5, isFull: false, trainer: nil, style: .onSite)
+
+print(training1.style.test())
+
+//rawValue can be getted
+training1.style.rawValue
+
+//Enums can be built using rawValues
+Training.Style(rawValue: 3)
+
+let me = Trainer(firstName: "Ludovic")
+training1.trainer = me
+
+let capName = training1.trainer?.firstName.uppercased()
+// let completeName = "M." + capName! // ! : force-unwrap operator
+let completeName = "M." + (capName ?? "No Trainer") // ?? : nil coalescing operator
+
+let completeName2: String
+if capName != nil { // test against nil
+    completeName2 = "M. \(capName!)"
+} else {
+    completeName2 = "No trainer yet"
+}
+
+let completeName3: String
+if let trainerName = capName { // if-let syntax (Optional binding)
+    completeName3 = "M. \(trainerName)"
+} else {
+    completeName3 = "No trainer yet"
+}
+
+let count = capName?.count
+
+guard let trainerName = capName else { fatalError() } // if-let syntax (Optional binding)
+trainerName
